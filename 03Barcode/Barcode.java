79530f599ec3f0ef94d41bbfa40aee26652354a6
Asim Kapparova <asimkapparova@Asims-MacBook-Pro.local>
2016-12-07 23:47:17 -0500

    Finished and tested

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -6,12 +6,14 @@ public class Barcode implements Comparable<Barcode>{
     String[] code = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
 
     private int checkSum(){
-	int x = 0;
-	for(int i = 0 ; i < _zip.length() ; i++){
-	    x += _zip.charAt(i);
+	int num = Integer.parseInt(_zip);
+	int sum = 0;
+	while (num > 0) {
+	    sum += num % 10;
+	    num /= 10;
 	}
-	x += _checkDigit;
-	return x;
+	int checkNum = sum % 10;
+	return checkNum;
     }
 
     public Barcode(String zip){
@@ -46,26 +48,4 @@ public class Barcode implements Comparable<Barcode>{
 	return toString().compareTo(other.toString());
     }
 
-    public static void main(String args[]){
-	Barcode b = new Barcode("08451");
-	Barcode c = new Barcode("99999");
-	Barcode d = new Barcode("11111");
-	System.out.println(b); //084518 |||:::|::|::|::|:|:|::::|||::|:|
-	System.out.println(b.toString().compareTo("084518 |||:::|::|::|::|:|:|::::|||::|:|")); //0
-	System.out.println(b.compareTo(b)); //0
-	System.out.println(c.compareTo(b));
-	System.out.println(d.compareTo(b));
-	/*length
-	  Barcode e = new Barcode("123456");
-	  System.out.println(e);
-	*/
-	/*length
-	  Barcode e = new Barcode("1234");
-	  System.out.println(e);
-	*/
-	/*type
-	  Barcode e= new Barcode("12.45");
-	  System.out.println(e);
-	*/
-    }   
 }
