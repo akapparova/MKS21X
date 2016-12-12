740544fdefa18e55f06e4c185664f407029f70a2
Asim Kapparova <asimkapparova@Asims-MacBook-Pro.local>
2016-12-12 04:58:02 -0500

    Finished

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -2,10 +2,10 @@ public class Barcode implements Comparable<Barcode>{
     
     private String _zip;
     
-    String[] code = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
+    private static String[] code = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
 
-    private int checkSum(){
-	int num = Integer.parseInt(_zip);
+    private static int checkSum(String zip){
+	int num = Integer.parseInt(zip);
 	int sum = 0;
 	while (num > 0) {
 	    sum += num % 10;
@@ -28,24 +28,83 @@ public class Barcode implements Comparable<Barcode>{
 	_zip = zip;
     }
 
+    public static String toCode(String zip){
+	String ans = "";
+	for (int i = 0; i < 5; i++){
+	    ans += code[Integer.parseInt(zip.substring(i, i + 1))];
+	}
+	ans += code[checkSum(zip)];
+	return "|" + ans + "|";
+    }
+
+	    
     public String toString(){
 	String ans = "";
 	for (int i = 0; i < 5; i++){
 	    ans += code[Integer.parseInt(_zip.substring(i, i + 1))];
 	}
-	ans += code[checkSum()];
-	return _zip + checkSum() + "       " + ans;
+	ans += code[checkSum(_zip)];
+	return _zip + checkSum(_zip) + " " + "|" + ans + "|";
     }
 
     public int compareTo(Barcode other){
 	return toString().compareTo(other.toString());
     }
 
-    public String toZip(String code){
-	String zip =  "";
-	for (int i = 0; i < code.length() - 5; i += 5){
-	    zip += code.charAt(i);
+    public static String toZip(String c){
+	String zipCode =  "";
+	if (c.length() != 32){
+	    throw new IllegalArgumentException("Barcode not correct length!");
 	}
-	return zip;
+	if (c.charAt(0) != '|' || c.charAt (c.length() - 1) != '|'){
+	throw new IllegalArgumentException("Barcode doesn't have guardrail(s)!");
+	}
+	for (int i = 0; i < c.length(); i ++){
+	    if (c.charAt(i) != '|' &&  c.charAt(i) != ':'){
+		throw new IllegalArgumentException("Barcode contains non-Barcode characters!");
+	    }
+	}
+	for(int i = 1; i < c.length() - 5; i += 5){
+		String num = c.substring(i, i+5);
+		switch(num){
+		case "||:::":
+		    zipCode += "0";
+		    break;
+		case ":::||":
+		    zipCode += "1";
+		    break;
+		case  "::|:|":
+		    zipCode += "2";
+		    break;
+		case "::||:":
+		    zipCode += "3";
+		    break;
+		case ":|::|":
+		    zipCode += "4";
+		    break;
+		case ":|:|:":
+		    zipCode += "5";
+		    break;
+		case ":||::":
+		    zipCode += "6";
+		    break;
+		case "|:::|":
+		    zipCode += "7";
+		    break;
+		case "|::|:":
+		    zipCode += "8";
+		    break;
+		case "|:|::":
+		    zipCode += "9";
+		    break;
+		default:
+		    throw new IllegalArgumentException("Invalid characters!");
+		}
+	    }
+	    if(checkSum(zipCode.substring(0, 5))%10 != Integer.parseInt(zipCode.substring(5))){
+		throw new IllegalArgumentException("Checksum doesn't Check out!");
+	    }
+	    return zipCode;
     }
 }
+
